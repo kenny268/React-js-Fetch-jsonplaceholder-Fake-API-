864e438c421c218f5eb6af4eb39075de2511513f
kenny268 <kentmutai5@gmail.com>
2023-01-23 14:00:07 +0300

    added custom hooks

---
+++ src/hooks/UseAPi.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+function useAPI(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+}
