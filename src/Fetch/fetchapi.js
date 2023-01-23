a2b8c4ec158d4306f8a6758969ec007a1192abf4
kenny268 <kentmutai5@gmail.com>
2023-01-23 13:38:39 +0300

    added fetch funtionality worked

--- src/Fetch/fetchapi.js
+++ src/Fetch/fetchapi.js
@@ -3,34 +3,27 @@ import { useState,useEffect } from "react";
 const  FetchApi = () => {
 
 const [data, setData] = useState([]);
+const [errors,setErrors]= useState(false);
 
 
 useEffect(() => {
-  const getData = async () => {
-    const url = `https://jsonplaceholder.typicode.com/posts`;
-   
 
-    try {
-      const resp = await fetch (url);
-      const data = await resp.json();
-      // Do anything you need to do to
-      // data before this call:
-      setData(data);
-    } catch (err) {
-      
-    } finally {
-    }
-  }
-
- 
+  fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res)=>res.json())
+  .then((data)=>setData(data))
+  .catch((err)=>setErrors)
+  
+  
 }, []);
       
-      
+console.log(errors)
     return ( 
 
         <div className="wrapper">
-            {data.length >0 && data.map((post)=>
             
+            
+            {data.length >0 && data.map((post)=>
+              
                 <div className="card" key={post.it}>
                     <h1>{post.title}</h1>
                     <p>{post.body}</p>
